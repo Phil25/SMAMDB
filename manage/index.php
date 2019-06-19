573541b3e1fdfab7cf57941da4f555fdef61ba22
Phil25 <Phil25@ProtonMail.com>
2019-06-19 17:36:31 +0200

    Mustard refactoring

--- manage/index.php
+++ manage/index.php
@@ -9,6 +9,7 @@ require(__DIR__ . "/../steamauth/steamauth.php");
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Manager</title>
+	<link rel="stylesheet" href="https://unpkg.com/mustard-ui@latest/dist/css/mustard-ui.min.css">
 </head>
 <body>
 <?php if(!isset($_SESSION["steamid"])) { ?>
--- manage/manager.php
+++ manage/manager.php
@@ -1,40 +1,14 @@
-<style>
-.tooltip {
-	position: relative;
-	display: inline-block;
-}
-.tooltip .tooltiptext {
-	visibility: hidden;
-	width: 500px;
-	background-color: LightGray;
-	padding: 10px;
-	border-style: solid;
-	border-width: 2px;
-	/* Position the tooltip */
-	position: absolute;
-	z-index: 1;
-	top: -5px;
-	left: 105%;
-}
-.tooltip:hover .tooltiptext {
-	visibility: visible;
-}
-.code {
-	font-family: monospace;
-	background-color: DarkGray
-}
-</style>
-
-<div style="max-width:500px;padding:20px;margin:20px">
+<div class="row">
+<div class="col-sm-6">
 
 <?php
 $addon = array();
 
 if(isset($_GET["id"]))
 {
-	$addon["id"]= $_GET["id"];
-	$idesc		= $db->real_escape_string($addon["id"]);
-	$addon_data	= $db->query("SELECT * FROM addons WHERE id='$idesc'")->fetch_assoc();
+	$addon["id"] = $_GET["id"];
+	$idesc = $db->real_escape_string($addon["id"]);
+	$addon_data = $db->query("SELECT * FROM addons WHERE id='$idesc'")->fetch_assoc();
 	if($addon_data)
 	{
 		if($addon_data["addedby"] == $_SESSION["steamid"])
@@ -46,49 +20,43 @@ if(isset($_GET["id"]))
 		}
 		else
 		{
-			$_POST["iderror"] = "No access: $idesc";
+			$_POST["iderror"] = "No access to addon: $idesc";
 		}
 	}
 	else
 	{
-		$_POST["iderror"] = "Not found: $idesc";
+		$_POST["iderror"] = "Addon not found: $idesc";
 	}
 }
 
+$iderror = isset($_POST["iderror"]);
+$editing = isset($_GET["id"]);
+
 ?>
 
-<form><fieldset>
-<legend>Load addon</legend>
-<input type="text" name="id" placeholder="Addon ID" required>
-<input type="submit" value="Load">
-<?php if(isset($_GET["id"]))
+<form action="/manage/process.php" method="post" style="padding-left:50px;padding-right:50px">
+<fieldset>
+<legend>Editing addon: <?php echo $addon["id"]; ?></legend>
+
+<?php
+if($iderror)
 {
-	if(!isset($_POST["iderror"]))
-	{
-		echo "Loaded: " . $_GET["id"];
-	}
-	else
-	{
-		echo $_POST["iderror"];
-	}
+	echo '<p class="alert alert-danger">' . $_POST["iderror"] . '</p>';
 }
 ?>
-</fieldset></form>
 
-<form action="/manage/process.php" method="post"><fieldset>
-<legend>Edit addon</legend>
+<div class="form-control-group">
+	<div class="form-control">
+		<label>Name <?php if($editing) echo " (locked for editing)" ?></label>
+		<input type="text" name="id" placeholder="Addon ID" autofocus <?php if($editing || $iderror) echo "readonly"; ?> required value="<?php echo $addon["id"] ?>">
+	</div>
 
-Name <input type="text" name="id" placeholder="Addon ID" style="float:right;" autofocus required value="<?php echo $addon["id"] ?>">
-<br><br>
-
-Author <input type="text" name="author" placeholder="Addon author" style="float:right;" required value="<?php echo $addon["author"] ?>">
-<br><br>
-
-Plugin ID
-<div class="tooltip">(?)
-	<span class="tooltiptext">Found on top of AlliedModders thread.</span>
+	<div class="form-control">
+		<label>Author</label>
+		<input type="text" name="author" placeholder="Addon author" required value="<?php echo $addon["author"] ?>">
+	</div>
 </div>
-<br>
+
 <?php
 if(isset($addon["forumid"]))
 {
@@ -102,55 +70,116 @@ else
 	$known_checked = "checked";
 }
 ?>
-<input type="radio" name="forumid" value="known" <?php echo $known_checked ?>>
+<fieldset>
+<legend>Plugin ID</legend>
+<input type="radio" name="forumid" value="known"<?php echo $known_checked; ?>>
 <input type="number" min="1" placeholder="Plugin ID" name="forumid_num" value="<?php echo $known_value; ?>"/><br>
 <input type="radio" name="forumid" value="extension"<?php echo $extension_checked; ?>>Extension<br>
-<input type="radio" name="forumid" value="unspecified"<?php echo $unspecified_checked; ?>>Unspecified<br>
-<br>
+<input type="radio" name="forumid" value="unspecified"<?php echo $unspecified_checked; ?>>Unspecified
+</fieldset>
 
+<div class="form-control">
+<label>
 Base URL
-<div class="tooltip">(?)
-	<span class="tooltiptext">
+</label>
+<span class="float-right tooltip">(?)
+	<span class="tooltip-text">
 		<p>URL which will be searched for the specified files.</p>
 		<p>AlliedModders forums and GitHub are treated individually. Information/examples on selecting base URL here TODO.</p>
 	</span>
+</span>
+<input type="text" name="url" placeholder="URL searched for files" value="<?php echo $addon["url"]; ?>" required>
 </div>
-<input type="url" name="url" placeholder="URL searched for files" style="width:100%" value="<?php echo $addon["url"]; ?>" required>
-<br><br>
 
 Files
-<div class="tooltip">(?)
-	<span class="tooltiptext"><ul>
+<span class="float-right tooltip">(?)
+	<span class="tooltip-text"><ul>
 		<li>Separated by newline.</li>
-		<li>Path and speparated by semicolon (<span class="code">;</span>)</li>
-		<li>Format: <span class="code">(path)/;(file)</span> (for SM root:<span class="code">./;(file)</span>).</li>
-		<li>Relative to <span class="code">(mod)/addons/sourcemod/</span>.</li>
-		<li>May go up two directories at most (up to <span class="code">(mod)/</span>).</li>
+		<li>Path and speparated by semicolon (<code>;</code>)</li>
+		<li>Format: <code>(path)/;(file)</code> (for SM root:<code>./;(file)</code>).</li>
+		<li>Relative to <code>(mod)/addons/sourcemod/</code>.</li>
+		<li>May go up two directories at most (up to <code>(mod)/</code>).</li>
 		<li>Archives are extracted preserving their inner directory structure.</li>
 		<li>Do not include directories.</li>
 		<li>View cohesive examples here TODO.</li>
 	</ul></span>
-</div>
+</span>
 <textarea name="files" cols="40" rows="5" placeholder="plugins/;thriller.smx
 gamedata/;thriller.plugin.txt
 plugins/;AdvancedInfinteAmmo.smx
 ./;funcommandsX_.*.zip
 ../../;.*" style="width:100%" required><?php echo $addon["files"]; ?></textarea>
-<br><br>
 
-<input type="submit" value="Add/Update">
-</fieldset></form>
+<?php
+	if(!$iderror)
+	{
+		echo '<br><input class="float-right" type="submit" value="' . ($editing ? "Update" : "Add") . '">';
+	}
+?>
+</fieldset>
+</form>
 
-<form><fieldset style="background-color:Crimson;">
-<legend style="background-color:white;">Delete addon</legend>
-<input type="text" name="delete" placeholder="Addon ID to delete" required>
-<input type="submit" value="Delete WITHOUT confirmation">
-</fieldset></form>
 
-<br><br>
+</div>
 
-Logged in as <?php echo $steamprofile["personaname"] . " -- " . $_SESSION["steamid"]; ?>
+<div class="col-sm-6">
 
-<div style="float:right;"><?php logoutbutton(); ?></div>
+<div style="padding-left:50px;padding-right:50px"><fieldset>
+<legend>Your addons</legend>
 
+<script>
+function filterAddons()
+{
+	let input = document.getElementById('filterInput');
+	let filter = input.value.toUpperCase();
+	let ul = document.getElementById('myTags');
+	let as = ul.getElementsByTagName('a');
+
+	let text;
+	for(let i = 0; i < as.length; ++i)
+	{
+		text = as[i].innerText.toUpperCase();
+		as[i].style.display = text.indexOf(filter) > -1 ? "" : "none";
+	}
+}
+</script>
+
+<div class="row">
+	<div class="col-sm-8">
+		<input type="text" id="filterInput" onkeyup="filterAddons()" placeholder="Filter...">
+	</div>
+	<div class="col-sm-4">
+		<center><form><input type="submit" value="New addon"></form></center>
+	</div>
+</div>
+
+<fieldset><ul class="tags" id="myTags">
+<?php
+$myaddons = $db->query("SELECT id, author FROM addons WHERE addedby='" . $_SESSION['steamid'] . "'");
+if($myaddons->num_rows > 0)
+{
+	while($row = $myaddons->fetch_assoc())
+	{
+		echo '<a class="tag tag-rounded" href="?id=' . $row['id'] . '">' . $row['id'] . '</a>';
+	}
+}
+?>
+</ul></fieldset>
+
+</fieldset></div>
+
+<div style="padding-left:50px;padding-right:50px"><fieldset>
+<legend>Login information</legend>
+
+<div class="row">
+<div class="col-sm-6">
+	Logged in as <?php echo $steamprofile["personaname"]; ?><br>
+	<?php echo $_SESSION["steamid"]; ?>
+</div>
+<div class="col-sm-6">
+	<div style="float:right;"><?php logoutbutton(); ?></div>
+</div>
+</div></fieldset></div>
+
+</div>
 </div>
