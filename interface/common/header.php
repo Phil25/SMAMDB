c9b5cbca8cfab1a5c649def9dce91612809740ea
Phil25 <Phil25@ProtonMail.com>
2019-06-30 14:01:16 +0200

    Header breadcrumbs

--- interface/common/header.php
+++ interface/common/header.php
@@ -1,7 +1,33 @@
+<?php
+$basename = basename($_SERVER["SCRIPT_FILENAME"], ".php");
+?>
+
 <section class="section-primary">
 	<div class="row">
 		<div class="col-md-9">
-			<h3>SMAMDB Interface</h3>
+			<h3>SMAM Database Interface</h3>
+			<ul class="breadcrumbs">
+				<?php
+				if($basename === "index")
+				{
+					echo '<li>interface home</li>';
+				}
+				else // in form
+				{
+					echo '<li><a href="/interface">interface home</a></li>';
+
+					$editing = $_GET["action"] === "edit";
+					if($editing)
+					{
+						echo '<li>editing ' . $_GET["id"] . '</li>';
+					}
+					else
+					{
+						echo '<li>new ' . ($_GET["table"] === "addons" ? "addon" : "appeal") . '</li>';
+					}
+				}
+				?>
+			</ul>
 		</div>
 		<div class="col-md-3 align-center">
 			Logged in as <strong><?php echo htmlspecialchars($steamprofile["personaname"]); ?></strong>
