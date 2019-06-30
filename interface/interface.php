775d39166485114d6fb0020dc63738387ff02162
Phil25 <Phil25@ProtonMail.com>
2019-06-30 14:30:32 +0200

    Hide adding addon button without access

--- interface/interface.php
+++ interface/interface.php
@@ -11,6 +11,9 @@ function spawnAddons($table)
 		echo '<a class="tag tag-rounded' . $green . '" style="margin:4px" href="form.php?table=' . $table . '&action=edit&id=' . $row["id"] . '">' . $row["id"] . '</a>';
 	}
 }
+
+DB::query("SELECT name FROM users WHERE steamid=" . $_SESSION["steamid"]);
+$access = sizeof(DB::getData()) > 0;
 ?>
 
 <section class="section-secondary">
@@ -20,12 +23,16 @@ function spawnAddons($table)
 			<h3 class="card-title">Your addons</h3>
 			<p>Your addons which have been accepted and are available for download by anyone.</p>
 			<div class="row">
-				<div class="col-sm-10">
+				<?php echo $access ? '<div class="col-sm-10">' : ''; ?>
 					<input type="text" id="filterAddons" onkeyup="filterItems('filterAddons', 'myAddons')" placeholder="Filter...">
-				</div>
-				<div class="col-sm-2">
-					<a href="form.php?table=addons&action=add"><button>+</button></a>
-				</div>
+				<?php
+					if($access)
+					{
+						echo '</div><div class="col-sm-2">'
+							. '<a href="form.php?table=addons&action=add"><button>+</button></a>'
+							. '</div>';
+					}
+				?>
 			</div>
 			<div style="height:300px;overflow:hidden;overflow-y:auto;">
 				<div class="tags" id="myAddons">
