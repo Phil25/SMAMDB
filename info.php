390851a2a516c874486bdcd6941b5e69063a2b0c
Phil25 <Phil25@ProtonMail.com>
2019-07-02 21:10:57 +0200

    Pull dependencies

--- info.php
+++ info.php
@@ -6,8 +6,7 @@ function printInfo($db, $ids)
 
 function getInfo($db, $ids)
 {
-	$list = "('" . implode("', '", explode(",", $ids)) . "')";
-	$sql = "SELECT id, url, files FROM addons WHERE id IN $list";
+	$sql = isset($_GET["nodeps"]) ? queryNoDeps($db, $ids) : queryDeps($db, $ids);
 	$res = $db->query($sql);
 
 	if($res->num_rows == 0)
@@ -24,11 +23,54 @@ function getInfo($db, $ids)
 	return $rows;
 }
 
+function queryNoDeps($db, $ids) : string
+{
+	$list = toSqlList(explode(",", $ids));
+	return "SELECT id, baseurl, files, games FROM addons WHERE id IN $list";
+}
+
+function queryDeps($db, $ids) : string
+{
+	$list = explode(",", $ids);
+	joinDeps($db, $list);
+	return "SELECT id, baseurl, files, games FROM addons WHERE id IN " . toSqlList($list);
+}
+
+function joinDeps($db, &$arr) // TODO: need an SQL wizz to turn this into a query
+{
+	$sql = "SELECT deps FROM addons WHERE id IN " . toSqlList($arr);
+	$res = $db->query($sql);
+	$newDepsFetched = false;
+
+	while($row = $res->fetch_assoc())
+	{
+		$deps = explode(" ", $row["deps"]);
+		if(sizeof($deps) < 1) continue;
+
+		foreach($deps as $dep)
+		{
+			if(strlen($dep) > 0 && !in_array($dep, $arr))
+			{
+				array_push($arr, $dep);
+				$newDepsFetched = true;
+			}
+		}
+	}
+
+	if($newDepsFetched) joinDeps($db, $arr);
+}
+
+function toSqlList($arr) : string
+{
+	return "('" . implode("', '", $arr) . "')";
+}
+
 function constructAddon($row)
 {
 	$addon = array();
 	$addon["id"] = $row["id"];
-	$addon["url"] = $row["url"];
+	$addon["url"] = $row["baseurl"];
 	$addon["files"] = explode("\r\n", $row["files"]);
+	$addon["games"] = explode(" ", $row["games"]);
 	return $addon;
 }
